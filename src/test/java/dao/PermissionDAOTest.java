1ab989c0c774ce0fd1d1018847a97d485bd16d18
upravaD <daktah@icloud.com>
2023-08-31 17:40:02 +0400

    add test

---
+++ src/test/java/dao/PermissionDAOTest.java
@@ -0,0 +1,144 @@
+package dao;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.project.dao.PermissionDAO;
+import org.project.model.Permission;
+import org.project.util.PostgresConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+class PermissionDAOTest {
+    private Connection connection;
+    private PermissionDAO permissionDAO;
+
+    @BeforeEach
+    public void setup() {
+        connection = PostgresConnection.getConnection();
+        permissionDAO = new PermissionDAO(connection);
+    }
+
+    @AfterEach
+    public void cleanup() throws SQLException {
+        connection.close();
+    }
+
+    @Test
+    void testCreatePermission() {
+        // Setup
+        String name = "TestCreatePermission";
+        Permission permission = new Permission();
+        permission.setPermissionName(name);
+
+        // Test
+        permissionDAO.create(permission);
+
+        // Verify
+        Permission createdPermission = permissionDAO.findByID(permission.getId());
+        Assertions.assertNotNull(createdPermission);
+        Assertions.assertEquals(name, createdPermission.getPermissionName());
+
+        // Cleanup
+        permissionDAO.delete(permission);
+    }
+
+    @Test
+    void testGetAllPermissions() {
+        // Setup
+        String name1 = "TestGetAllPermission1";
+        String name2 = "TestGetAllPermission2";
+        List<Permission> beforePermissions = permissionDAO.getALL();
+        Permission permission1 = new Permission();
+        permission1.setPermissionName(name1);
+        Permission permission2 = new Permission();
+        permission2.setPermissionName(name2);
+        permissionDAO.create(permission1);
+        permissionDAO.create(permission2);
+
+        // Test
+        List<Permission> permissions = permissionDAO.getALL();
+
+        // Verify
+        Assertions.assertNotNull(permissions);
+        Assertions.assertEquals(beforePermissions.size() + 2, permissions.size());
+
+        // Cleanup
+        permissionDAO.delete(permission1);
+        permissionDAO.delete(permission2);
+    }
+
+    @Test
+    void testFindPermissionByID() {
+        // Setup
+        Permission permission = new Permission();
+        permission.setPermissionName("TestFindByIDPermission");
+        permissionDAO.create(permission);
+
+        // Test
+        Permission foundPermission = permissionDAO.findByID(permission.getId());
+
+        // Verify
+        Assertions.assertNotNull(foundPermission);
+        Assertions.assertEquals(permission.getId(), foundPermission.getId());
+        Assertions.assertEquals(permission.getPermissionName(), foundPermission.getPermissionName());
+
+        // Cleanup
+        permissionDAO.delete(permission);
+    }
+
+    @Test
+    void testFindNonExistentPermission() {
+        // Test
+        Permission nonExistentPermission = permissionDAO.findByID(-1L);
+
+        // Verify
+        Assertions.assertNotNull(nonExistentPermission);
+        Assertions.assertEquals(-1L, nonExistentPermission.getId());
+        Assertions.assertEquals("emptyPermissionName", nonExistentPermission.getPermissionName());
+    }
+
+    @Test
+    void testUpdatePermission() {
+        // Setup
+        Permission permission = new Permission();
+        permission.setPermissionName("TestOldPermission");
+        permissionDAO.create(permission);
+
+        Permission updatedPermission = new Permission();
+        updatedPermission.setId(permission.getId());
+        updatedPermission.setPermissionName("TestNewPermission");
+
+        // Test
+        permissionDAO.update(updatedPermission);
+
+        // Verify
+        Permission retrievedPermission = permissionDAO.findByID(permission.getId());
+        Assertions.assertNotNull(retrievedPermission);
+        Assertions.assertEquals(updatedPermission.getPermissionName(), retrievedPermission.getPermissionName());
+
+        // Cleanup
+        permissionDAO.delete(updatedPermission);
+    }
+
+    @Test
+    void testDeletePermission() {
+        // Setup
+        Permission permission = new Permission();
+        permission.setPermissionName("TestDeletePermission");
+        permissionDAO.create(permission);
+
+        // Test
+        permissionDAO.delete(permission);
+
+        // Verify
+        Permission deletedPermission = permissionDAO.findByID(permission.getId());
+        Assertions.assertNotNull(deletedPermission);
+        Assertions.assertEquals(-1L, deletedPermission.getId());
+        Assertions.assertEquals("emptyPermissionName", deletedPermission.getPermissionName());
+    }
+}
---
+++ src/test/java/dao/RoleDAOTest.java
@@ -0,0 +1,178 @@
+package dao;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.project.dao.PermissionDAO;
+import org.project.dao.RoleDAO;
+import org.project.model.Permission;
+import org.project.model.Role;
+import org.project.util.PostgresConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+class RoleDAOTest {
+    private Connection connection;
+    private RoleDAO roleDAO;
+
+    @BeforeEach
+    public void setup() {
+        connection = PostgresConnection.getConnection();
+        roleDAO = new RoleDAO(connection);
+    }
+
+    @AfterEach
+    public void cleanup() throws SQLException {
+        connection.close();
+    }
+
+    @Test
+    void testCreateRole() {
+        // Setup
+        Role role = new Role();
+        role.setRoleName("TestRole");
+
+        // Test
+        roleDAO.create(role);
+
+        // Verify
+        Role createdRole = roleDAO.findByID(role.getId());
+        Assertions.assertNotNull(createdRole);
+        Assertions.assertEquals(role.getRoleName(), createdRole.getRoleName());
+
+        // Cleanup
+        roleDAO.delete(role);
+    }
+
+    @Test
+    void testGetAllRoles() {
+        // Setup
+        List<Role> beforeRoles = roleDAO.getALL();
+
+        Role role1 = new Role();
+        role1.setRoleName("Role1");
+        roleDAO.create(role1);
+
+        Role role2 = new Role();
+        role2.setRoleName("Role2");
+        roleDAO.create(role2);
+
+        // Test
+        List<Role> roles = roleDAO.getALL();
+
+        // Verify
+        Assertions.assertEquals(beforeRoles.size() + 2, roles.size());
+
+        // Cleanup
+        roleDAO.delete(role1);
+        roleDAO.delete(role2);
+    }
+
+    @Test
+    void testFindRoleByID() {
+        // Setup
+        Role role = new Role();
+        role.setRoleName("TestRole");
+        roleDAO.create(role);
+
+        // Test
+        Role retrievedRole = roleDAO.findByID(role.getId());
+
+        // Verify
+        Assertions.assertNotNull(retrievedRole);
+        Assertions.assertEquals(role.getRoleName(), retrievedRole.getRoleName());
+        Assertions.assertEquals(role.getId(), retrievedRole.getId());
+
+        // Cleanup
+        roleDAO.delete(role);
+    }
+
+    @Test
+    void testUpdateRole() {
+        // Setup
+        Role role = new Role();
+        role.setRoleName("OldRole");
+        roleDAO.create(role);
+
+        Role updatedRole = new Role();
+        updatedRole.setId(role.getId());
+        updatedRole.setRoleName("NewRole");
+
+        // Test
+        roleDAO.update(updatedRole);
+
+        // Verify
+        Role retrievedRole = roleDAO.findByID(role.getId());
+        Assertions.assertNotNull(retrievedRole);
+        Assertions.assertEquals(updatedRole.getRoleName(), retrievedRole.getRoleName());
+
+        // Cleanup
+        roleDAO.delete(role);
+        roleDAO.delete(updatedRole);
+    }
+
+    @Test
+    void testDeleteRole() {
+        // Setup
+        Role role = new Role();
+        role.setRoleName("TestRole");
+        roleDAO.create(role);
+
+        // Test
+        roleDAO.delete(role);
+
+        // Verify
+        Role deletedRole = roleDAO.findByID(role.getId());
+        Assertions.assertNotNull(deletedRole);
+        Assertions.assertEquals(-1L, deletedRole.getId());
+        Assertions.assertEquals("emptyRoleName", deletedRole.getRoleName());
+    }
+
+    @Test
+    void testSetRolePermission() {
+        // Setup
+        Role role = new Role();
+        role.setRoleName("TestRole");
+        roleDAO.create(role);
+
+        PermissionDAO permissionDAO = new PermissionDAO(connection);
+        Permission permission = new Permission();
+        permission.setPermissionName("TestPermission");
+        permissionDAO.create(permission);
+
+        // Test
+        roleDAO.setRolePermission(role, permission);
+
+        // Verify
+        Role updatedRole = roleDAO.findByID(role.getId());
+        Assertions.assertNotNull(updatedRole);
+        List<Permission> permissions = updatedRole.getPermissions();
+        Assertions.assertTrue(permissions.size() > 0);
+    }
+
+    @Test
+    void testDeleteRolePermission() {
+        // Setup
+        Role role = new Role();
+        role.setRoleName("TestRole");
+        roleDAO.create(role);
+
+        PermissionDAO permissionDAO = new PermissionDAO(connection);
+        Permission permission = new Permission();
+        permission.setPermissionName("TestPermission");
+        permissionDAO.create(permission);
+
+        // Test
+        roleDAO.deleteRolePermission(role);
+
+        // Verify
+        Role updatedRole = roleDAO.findByID(role.getId());
+        Assertions.assertNotNull(updatedRole);
+        List<Permission> permissions = updatedRole.getPermissions();
+        Assertions.assertTrue(permissions.size() > 0);
+    }
+}
